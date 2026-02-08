1b9c64f5cabe0c8d9e768e5032dd1c7c5f275a60
DTN101 <dileepkumar.tentu@serviceaide.com>
2026-02-08 21:08:08 +0530

    project details.

---
+++ src/component/FormAddBal.jsx
@@ -0,0 +1,49 @@
+import { useSnackbar } from 'notistack'
+import React, { useState } from 'react'
+
+const FormAddBal = ({onClose,  setBalance}) => {
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar()
+  const [addbal, setAddbal] =useState("")
+  const handleInput = (value)=>{
+    const regex = /^[0-9]+$/;
+   if (regex.test(value) || value === "") {
+    setAddbal(value);
+  } else {
+    enqueueSnackbar("use number only", { variant: "warning" })
+   /// alert("use number only");
+    setAddbal("");
+  }
+    
+  }
+
+  const addBalence = ()=>{
+     const storedBalance = localStorage.getItem("balance") || "0";
+     // console.log(storedBalance);
+      const newTotal = Number(storedBalance) + Number(addbal);
+      //console.log(newTotal);    
+       setBalance(prev=> Number(prev) + Number(addbal))
+       localStorage.setItem("balance", newTotal);
+        setAddbal("");
+        enqueueSnackbar("Balance added", { variant: "success" })
+        onClose(false)
+  }
+  
+  const closeHanler = ()=>{
+    setAddbal("");
+     onClose(false)
+  }
+   return (
+     <div className=' text-amber-950'>
+         <h3 className=' text-3xl font-bold'>Add Balence</h3>
+         <div className='inputwrp'>
+             <input type='number' className='input' placeholder='Income Amount' value={addbal} onChange={(e)=>handleInput(e.target.value)}  />
+         </div>
+         <div className='btnwrp'>
+             <button type="submit" className='addbth' onClick={addBalence}>Add Balance</button>
+             <button className='canbtn' onClick={closeHanler}>Cancel</button>
+         </div>
+     </div>
+   )
+}
+
+export default FormAddBal
---
+++ src/pages/ExpencetrackerPage.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react'
+import Mainexpence from '../component/Mainexpence'
+import RecentExpence from '../component/RecentExpence'
+import Topexpence from '../component/Topexpence'
+
+const ExpencetrackerPage = () => {
+      const [totalExp, setTotalExp] = useState(() => {
+  try {
+    return JSON.parse(localStorage.getItem("expenses")) || [];
+  } catch {
+    return [];
+  }
+});
+const [balance, setBalance] = useState(() =>
+  Number(localStorage.getItem("balance")) || 5000
+);
+
+
+      const [topexpnc, setTopexpnc] = useState([])
+       const [espenceData, setEspenceData] =useState({
+    title: "",
+    price: "",
+    category: "",
+    date: ""
+  })
+  useEffect(() => {
+  localStorage.setItem("balance", balance);
+}, [balance]);
+           const catall = [...new Set(totalExp?.map(item => item.category))]  
+       //console.log(catall);
+
+  //  useEffect(()=>{
+  //    const storage = ()=>{
+  //    setTotalExp( JSON.parse(localStorage.getItem('expenses')) ) 
+  //    }
+  //    storage()
+  //  },[])
+  useEffect(() => {
+  localStorage.setItem("expenses", JSON.stringify(totalExp));
+}, [totalExp]);
+   //console.log(totalExp);
+
+         useEffect(()=>{
+   function createTopexpence(){
+ const expData = catall.map((cat) => {
+         let presentval = totalExp?.filter((menu) => menu.category === cat)
+         .reduce((previousValue, currentValue) => previousValue + Number(currentValue.price), 0)
+          return {"catNam": cat, "total":presentval}
+       })
+       console.log("expData", expData);
+       
+      setTopexpnc(expData) 
+}     
+createTopexpence()
+      },[totalExp])
+
+console.log("show topexp", topexpnc);
+
+
+  return (
+    <div className=' flex-1 flex flex-col'>
+           <h1>Expense Tracker</h1>
+           <Mainexpence balance={balance} setBalance={setBalance} expnc={totalExp} setTotalExp={setTotalExp} espenceData={espenceData} setEspenceData={setEspenceData} catall={catall} topexpnc={topexpnc}/>
+           <div className=' flex gap-2 flex-1 flex-wrap justify-between '>
+            <RecentExpence expnc={totalExp} setTotalExp={setTotalExp} balance={balance} setBalance={setBalance}/> <Topexpence topexpnc={topexpnc}/>
+           </div>
+    </div>
+  )
+}
+
+export default ExpencetrackerPage
